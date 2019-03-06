775a8e956d2c270cc4780d404a71445093245d07
Muhammad Sumair <sumair.sagitarius@gmail.com>
2019-03-07 01:55:59 +0500

    build star component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image, Platform} from 'react-native';
+import { Text, View, Image, Platform, TouchableOpacity} from 'react-native';
 import CompanyProfileStyle from './HeaderStyle';
 import StarRating from "react-native-star-rating/StarRating";
 
@@ -12,7 +12,6 @@ const Header = (props) => {
                     <View style={CompanyProfileStyle.profileHeaderWrapper}>
                         <Image
                             source={require('../../assets/images/ProfilePic.png')}
-                            // indicator={ProgressBar}
                             borderRadius={(Platform.OS === 'ios') ? 100 / 2 :  100}
                             style={CompanyProfileStyle.profileHeaderImage}/>
                     </View>
@@ -20,14 +19,23 @@ const Header = (props) => {
                 <View style={CompanyProfileStyle.profileText}>
                     <Text style={[CompanyProfileStyle.profileName]}>Anthony Martial</Text>
                     <Text style={{color: '#fcfeff', fontSize:16, marginVertical: 15}}>Barber With 9 Year of Experience</Text>
-                    <View style={{flexDirection: 'row'}}>
+                    <View style={{flexDirection: 'row', alignItems: 'center'}}>
                     <StarRating
                         disabled={false}
-                        starSize={25}
+                        starSize={15}
                         maxStars={5}
-                        rating={3.5}/>
-                        <Text>(17 Reviews)</Text>
+                        rating={3.5}
+                        fullStarColor={'#eb8b10'}
+                        emptyStarColor={'#a15e31'}
+
+                    />
+                        <Text style={{color: '#fcfeff', marginHorizontal: 5}}>(17 Reviews)</Text>
                     </View>
+                </View>
+                <View style={{alignItems:'center', marginTop: 20}}>
+                    <TouchableOpacity style={{backgroundColor:'#fe0000', borderRadius:50, height: 30, width: 155, alignItems:'center', justifyContent: 'center'}}>
+                        <Text style={{fontSize:15, color:'#ffdcec', fontWeight: 'bold'}}>View Profile</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
